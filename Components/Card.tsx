d0c11132dbffe892313939994f6c14b22dd1311b
TonyMike <tee.jhay1@gmail.com>
2023-05-24 13:45:04 +0100

    partners in progress

--- Components/Card.tsx
+++ Components/Card.tsx
@@ -1,7 +1,11 @@
+import Button from "./Button";
+
 const Card = () => {
   return (
-    <div className="bg-gray-100">
-      card
+    <div className="bg-gray-100 border  p-5 border-gray-300  rounded-lg min-w-[350px] space-y-6">
+      <h3 className="text-black font-semibold">Affiliate Program Management</h3>
+      <p className="text-gray-500 text-sm">Affiliate program management is the process of overseeing and developing a brand’s affiliate marketing program. Most noteworthy, the “outsource” approach towards managing your affiliate program is effectively the best approach.</p>
+      <Button text="Learn More" />
     </div>
   );
 }
--- Components/Partners/Partners.tsx
+++ Components/Partners/Partners.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Button from "../Button";
+import Card from "../Card";
 
 
 const Partners = () => {
@@ -22,6 +23,27 @@ const Partners = () => {
         <div className=" mt-10 py-5">
           <h3 className="text-black text-4xl text-center capitalize font-semibold mb-4">Modern Marketing and design that works</h3>
           <p className="text-gray-600 text-sm text-center leading-6">We are committed to providing results that go beyond what you envisioned, enabling your business to <br /> flourish and succeed in the digital realm.</p>
+        </div>
+      </div>
+
+      <div className="flex space-x-4 overflow-x-hidden w-full flex-nowrap px-10 mt-14 py-3">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+
+      </div>
+
+      <div className="bg-black p-20">
+        <div className="flex items-end">
+          <h2 className="text-4xl leading-10 w-2/3">
+            Empowering You to<br />
+            Thrive in the Competitive <br />
+            World of Digital Marketing"
+          </h2>
+
+          <p className="w-1/3">Partner with Gratis Digital and unlock the power of targeted leads to propel your business to new heights. Amplify your digital marketing success and stay ahead of the competition.</p>
         </div>
       </div>
 
